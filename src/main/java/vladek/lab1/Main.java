e60d4b2ed38ddf116ecdbfd6329e6d8e953d92a1
Vladislav <senior.vlados97@gmail.com>
2025-02-11 20:28:32 +0300

    Finish simplex-table calculation method

--- src/main/java/vladek/lab1/Main.java
+++ src/main/java/vladek/lab1/Main.java
@@ -14,23 +14,40 @@ public class Main {
         System.out.println("Введите количество переменных функции:");
         int n = sc.nextInt();   // общее количество переменных
         List<Integer> basis = new ArrayList<>();
-        int[] F = new int[n];
+        double[] F = new double[n];
 
         // Ввод значений переменных хj и определение базисных переменных
         for (int i = 0; i < n; i++) {
-            System.out.printf("Введите переменную значение переменной x%d = ", i + 1);
-            F[i] = sc.nextInt();
+            System.out.printf("Введите коэффициент переменной x%d = ", i + 1);
+            F[i] = -1 * sc.nextInt();
 
             if (F[i] == 0) {
                 basis.add(i + 1);
             }
         }
 
-        int[][] table = getSimplexTable(sc, F, basis);
+        double[][] table = getSimplexTable(sc, F, basis);
+//        double[][] table = new double[][]{
+//                {2, 1, -3, 0, 0, -6, 18},
+//                {-3, 0, 2, 1, 0, -2, 24},
+//                {1, 0, 3, 0, 1, -4, 36},
+//                {-3, 0, -2, 0, 0, 6, 0}
+//        };
+        System.out.println("Изначальная симплекс-таблица");
+        printTable(table);
 
         while (!isOptimal(table)) {
             int col = pivotColumnIndex(table);
             int row = pivotRowIndex(table, col);
+
+            if (row < 0) {
+                System.out.println("Функция не ограничена. Оптимальное решение отсутствует");
+                break;
+            }
+
+            recalculateTable(table, row, col);
+            System.out.printf("%nПересчитанная симплекс-таблица%n");
+            printTable(table);
         }
 
         sc.close();
@@ -38,18 +55,19 @@ public class Main {
 
     /**
      * Заполнят симплекс-таблицу начальными данными
-     * @param sc для считывания значений
-     * @param F функция
+     *
+     * @param sc    для считывания значений
+     * @param F     функция
      * @param basis позиции базисных переменных
      * @return симплекс-таблица
      */
-    public static int[][] getSimplexTable(Scanner sc, int[] F, List<Integer> basis) {
-        // Ввод начальной симплекс-таблицы
+    public static double[][] getSimplexTable(Scanner sc, double[] F, List<Integer> basis) {
         // Количество строк: количество базисных переменных + 1
         // Количество столбцов: количество переменных функции + 1
-        int[][] result = new int[basis.size() + 1][F.length + 1];
+        double[][] result = new double[basis.size() + 1][F.length + 1];
 
         for (int i = 0; i < result.length - 1; i++) {
+            System.out.printf("Введите %d элементов %d строки симлекс-таблицы%n", F.length + 1, i + 1);
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = sc.nextInt();
             }
@@ -63,10 +81,11 @@ public class Main {
 
     /**
      * Проверка решения на оптимальность
+     *
      * @param table симплекс-таблица
      * @return {@code true}, если в решении нет отрицательных значений. Иначе {@code false}
      */
-    public static boolean isOptimal(int[][] table) {
+    public static boolean isOptimal(double[][] table) {
         for (int i = 0; i < table[0].length; i++) {
             if (table[0][i] < 0) {
                 return false;
@@ -78,15 +97,16 @@ public class Main {
 
     /**
      * Определение ведущего столбца симплекс-таблицы
+     *
      * @param table симплекс-таблица
      * @return индекс столбца
      */
-    public static int pivotColumnIndex(int[][] table) {
-        int min = table[table.length - 1][0];
+    public static int pivotColumnIndex(double[][] table) {
+        double min = table[table.length - 1][0];
         int minIndex = 0;
 
         for (int i = 1; i < table[table.length - 1].length; i++) {
-            if (min < table[table.length - 1][i]) {
+            if (min > table[table.length - 1][i]) {
                 min = table[table.length - 1][i];
                 minIndex = i;
             }
@@ -97,23 +117,66 @@ public class Main {
 
     /**
      * Определение ведущей строки симплекс-таблицы
+     *
      * @param table симплекс-таблица
-     * @param col индекс ведущего столбца
+     * @param col   индекс ведущего столбца
      * @return индекс ведущей строки
      */
-    public static int pivotRowIndex(int[][] table, int col) {
-        int min = table[0][table[0].length - 1] / table[0][col];
-        int minIndex = 0;
+    public static int pivotRowIndex(double[][] table, int col) {
+        Double min = null;
+        int minIndex = -1;
 
-        for (int i = 1; i < table[table.length - 1].length; i++) {
-            int temp = table[i][table[0].length - 1] / table[i][col];
+        for (int i = 0; i < table.length; i++) {
+            double temp = table[i][table[0].length - 1] / table[i][col];
 
-            if (min < temp) {
+            if (temp <= 0.0 || temp <= -0.0) continue;
+
+            if (min == null || min > temp) {
                 min = temp;
                 minIndex = i;
             }
         }
 
         return minIndex;
+    }
+
+    /**
+     * Пересчитывает симплекс-таблицу
+     *
+     * @param table симплекс-таблица
+     * @param col   ведущий столбец
+     * @param row   ведущая строка
+     */
+    public static void recalculateTable(double[][] table, int row, int col) {
+        // Определяем минимальный элемент в строке F
+//        double min = table[table.length - 1][col];
+
+        // Определяем ведущий элемент
+        double pivotElement = table[row][col];
+
+        // Делим на него всю ведущую строку
+        for (int i = 0; i < table[row].length; i++) {
+            table[row][i] /= pivotElement;
+        }
+
+        // Обнуляем остальные элементы
+        for (int i = 0; i < table.length; i++) {
+            if (i == row) continue;
+
+            double pivotColumnElement = table[i][col];
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = table[i][j] - pivotColumnElement * table[row][j];
+            }
+        }
+    }
+
+    public static void printTable(double[][] table) {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.printf("%8.1f", table[i][j]);
+            }
+
+            System.out.println();
+        }
     }
 }
